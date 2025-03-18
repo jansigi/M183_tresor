df6063551ccdd465132bfbff3835906ab6b2e1f1
Jan Sigrist <jan.sigrist@ergon.ch>
2025-03-18 11:32:31 +0100

    Initial commit

---
+++ backend/src/main/java/ch/bbw/pr/tresorbackend/TresorbackendApplication.java
@@ -0,0 +1,11 @@
+package ch.bbw.pr.tresorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TresorbackendApplication {
+   public static void main(String[] args) {
+      SpringApplication.run(TresorbackendApplication.class, args);
+   }
+}
---
+++ backend/src/main/java/ch/bbw/pr/tresorbackend/model/NewSecret.java
@@ -0,0 +1,28 @@
+package ch.bbw.pr.tresorbackend.model;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * NewSecret
+ * @author Peter Rutschmann
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewSecret {
+   @NotEmpty (message="email is required.")
+   private String email;
+
+   @NotNull (message="secret is required.")
+   private JsonNode content;
+
+   @NotEmpty (message="encryption password id is required.")
+   private String encryptPassword;
+}
